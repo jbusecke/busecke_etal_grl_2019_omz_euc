30e6097964978080a9762ea176a1ffa5239c4459
Julius Busecke <jbusecke@princeton.edu>
2020-05-07 08:55:57 -0400

    Replaced name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='busecke_etal_grl_2019_omz_euc',
     packages=find_packages(),
     version='0.1.0',
     description='Code repository to reproduce results from Busecke_etal 2019 published in GRL',
